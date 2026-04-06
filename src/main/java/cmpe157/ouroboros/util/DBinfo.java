133d56fb9624b1e28cf6b1b44ec070eda687c2a1
Vance <vancednguyen@gmail.com>
2026-04-05 23:27:55 -0700

    test

--- src/main/java/cmpe157/ouroboros/util/DBinfo.java
+++ src/main/java/cmpe157/ouroboros/util/DBinfo.java
@@ -2,7 +2,7 @@ package cmpe157.ouroboros.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DBinfo { //change the user and password to yourswd
+public class DBinfo { //change the user and password to yours
     private static final String URL = "jdbc:mysql://localhost:3306/ouroboro_ev";
     private static final String USER = "root";
     private static final String PASSWORD = "4200";
